9d926bb154737c6ae77461d7369cbf8a4bcf8fe1
Yasser Gonzalez <yasserglez@gmail.com>
2015-09-25 18:42:41 -0400

    Load SleepBot.csv in server.R

--- sleep_app/server.R
+++ sleep_app/server.R
@@ -1,4 +1,19 @@
 library("shiny")
+library("readr")
+library("dplyr")
+library("lubridate")
+library("ggplot2")
+
+# Load and tidy the data exported from https://www.mysleepbot.com
+sleep_data <- read_delim("SleepBot.csv", ",",  quote = "'", col_types = "cc_c_") %>%
+    mutate(# SleepBot' Date is when the entry was created, but I want the clock-in date.
+        sleep_time = mdy_hm(paste(Date, `Sleep Time`)) - days(1),
+        duration = as.interval(hm(Duration), sleep_time)) %>%
+    # Combine non-contiguous entries.
+    group_by(as.Date(sleep_time)) %>%
+    summarize(sleep_time = min(sleep_time),
+              duration = sum(duration)) %>%
+    select(sleep_time, duration)
 
 shinyServer(function(input, output) {
 })
