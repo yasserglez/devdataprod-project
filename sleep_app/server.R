15ac99360ccd93d908f2cec50dc317853551c148
Yasser Gonzalez <yasserglez@gmail.com>
2015-09-25 20:46:54 -0400

    Add a first plot

--- sleep_app/server.R
+++ sleep_app/server.R
@@ -4,7 +4,8 @@ library("dplyr")
 library("lubridate")
 library("ggplot2")
 
-# Load and tidy the data exported from https://www.mysleepbot.com
+# Load and tidy the data exported from https://www.mysleepbot.com:
+
 sleep_data <- read_delim("SleepBot.csv", ",",  quote = "'", col_types = "cc_c_") %>%
     mutate(# SleepBot' Date is when the entry was created, but I want the clock-in date.
         sleep_time = mdy_hm(paste(Date, `Sleep Time`)) - days(1),
@@ -15,12 +16,31 @@ sleep_data <- read_delim("SleepBot.csv", ",",  quote = "'", col_types = "cc_c_")
               duration = sum(duration)) %>%
     select(sleep_time, duration)
 
-# Precompute information used in the plots
+
+# Precompute information used in the plots:
+
+min_date <- min(as.Date(sleep_data$sleep_time))
+max_date <- max(as.Date(sleep_data$sleep_time))
+
 sleep_data <- sleep_data %>%
-    mutate(date = as.Date(sleep_time))
+    mutate(date = as.Date(sleep_time), # just the date
+           duration = duration / 60 / 60) %>% # convert to hours
+    # Add NA entries for the days without data.
+    right_join(data.frame(date = seq(min_date, max_date, by = "1 day")), by = "date")
+
 
 shinyServer(function(input, output, session) {
-    updateDateRangeInput(session, "dateRange",
-                         min = min(sleep_data$date), max = max(sleep_data$date),
-                         start = min(sleep_data$date), end = max(sleep_data$date))
+    updateDateRangeInput(session, "dates",
+                         min = min_date, max = max_date,
+                         start = min_date, end = max_date)
+
+    plot_data <- reactive({
+        sleep_data %>%
+            filter(date >= input$dates[1], date <= input$dates[2])
+    })
+
+    output$duration_plot <- renderPlot({
+        ggplot(plot_data(), aes(x = date, y = duration)) +
+            geom_line()
+    })
 })
--- sleep_app/ui.R
+++ sleep_app/ui.R
@@ -1,26 +1,24 @@
 library("shiny")
 library("shinydashboard")
 
+plots_tab <- tabItem(tabName = "plots", fluidRow(
+    box(title = "Sleep Duration", width = 12, collapsible = TRUE, status = "primary", plotOutput("duration_plot"))
+))
+
+docs_tab <- tabItem(tabName = "docs", fluidRow(
+    box(title = "Documentation", width = 12, status = "primary")
+))
+
 dashboardPage(
     dashboardHeader(title = "Track Your Sleep!"),
     dashboardSidebar(
         sidebarMenu(
             menuItem("Plots", tabName = "plots", icon = icon("area-chart")),
-            dateRangeInput("dateRange", label = "Date Range:", startview = "year"),
+            dateRangeInput("dates", label = "Date Range:", startview = "year"),
             menuItem("Documentation", tabName = "docs", icon = icon("book")),
             menuItem("Source Code", icon = icon("github"),
                      href = "https://github.com/yasserglez/devdataprod-project")
         )
     ),
-    dashboardBody(
-        tabItems(
-            tabItem(tabName = "plots",
-                h2("Plots")
-            ),
-
-            tabItem(tabName = "docs",
-                h2("Documentation")
-            )
-        )
-    )
+    dashboardBody(tabItems(plots_tab, docs_tab))
 )
