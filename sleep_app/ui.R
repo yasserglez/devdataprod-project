89637299de64d18ab547340e5b8f144e4015f264
Yasser Gonzalez <yasserglez@gmail.com>
2015-09-26 19:47:04 -0400

    Fix typos in the Shiny app

--- sleep_app/ui.R
+++ sleep_app/ui.R
@@ -2,7 +2,7 @@ library("shiny")
 library("shinydashboard")
 
 dashboardPage(
-    dashboardHeader(title = "Sleep Tracking"),
+    dashboardHeader(title = "Sleep Monitor"),
     dashboardSidebar(
         sidebarMenu(
             dateRangeInput("dates", label = "Date Range:", startview = "year"),
@@ -14,13 +14,13 @@ dashboardPage(
         fluidRow(
             box(title = "Welcome", width = 12, collapsible = TRUE,
                 HTML(paste('<p>This <a href="http://shiny.rstudio.com/">Shiny</a> app',
-                           'allows you to analyze your sleeping pattern.')),
+                           'allows you to study your sleeping habits.')),
                 p(paste("The first plot shows each day's sleep duration, along with",
                         "a regression line and a 95% confidence interval. The second",
                         "and third plots are histograms of the sleep and wake time.",
                         "You can use the date picker widgets located in the sidebar",
                         "to select different date ranges.")),
-                HTML(paste('The sample data are my own records, collected the past year using',
+                HTML(paste('The sample data are my own records, collected over the past year using',
                            '<a href="https://mysleepbot.com/">SleepBot</a>.')))
         ),
         fluidRow(box(title = "Sleep Duration", width = 12, plotOutput("duration_plot"))),
